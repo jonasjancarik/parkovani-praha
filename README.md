a46d677f49801759a8bbcc407e1a87b4ddf53919
Jonáš Jančařík <jonas.jancarik@gmail.com>
2024-01-02 23:51:33 +0100

    improve download logic, describe files

--- README.md
+++ README.md
@@ -1,2 +1,64 @@
 # parkovani-praha
- 
+
+## Co znamenají názvy souborů?
+
+### JS a JSON soubory
+
+.js soubory obsahují popis JSON souborů, které se načítají do mapy.
+
+Tyto soubory ale obsahují podobná data jako .tsv soubory, narozdíl od nich jsou ale rozděleny časově (den, noc atd.). Navzdory tomu, že popis v .js souboru odkaazuje na .tsv soubor, tak tyto soubory zřejmě nemohly být vygenerovány jen z .tsv soborů (právě proto, že v těch chybí časové rozdělení).
+
+Název souborů se skládá z několika částí:
+- typ zobrazení v mapě (OB = Obsazenost, OR = Rezidenti, RE = Respektovanost) - různá zobrazení, ale stejná data
+- _
+- rok (2023)
+- měsíc (01-12)
+- časový rámec - den/noc/víkend (D = Den, N = Noc, P = Pondělí-Pátek mimo provozní dobu, S = Sobota-Neděle mimo provozní dobu, W = Pondělí-Pátek v provozní dobu, X = Sobota-Neděle v provozní dobu)
+- _
+- perioda dat - měsíční/čtvrtletní (N = Měsíční, Q = Čtvrtletní, P = ???)
+- úseky/základní sídelní jednotky (A, H = úseky, J = ZSJ)
+
+NA a PH, respektive QA a QH jsou stejné, mají jen jinak definované barvy a škály. Verze PH a QH budeme ignorovat.
+
+MPD znamená mimo pracovní dobu
+
+### TSV soubory
+
+P10-202308B_4.tsv
+
+- _4.tsv - úseky
+- _6.tsv - základní sídelní jednotky (oblasti)
+
+TW_202201X_7A.tsv
+
+- účel není jasný, nějaká definice úseků?
+
+### Jaké soubory jsou použité pro analýzu?
+
+Protože nás nezajímá zobrazení v mapě, ale pouze data, tak budeme používat jen jeden typ souborů - arbitrárně volíme soubory začínající OB_.
+
+Relevantní datové soubory pro jeden měsíc tedy vypadají takto:
+
+- OB_202311D_NA.json
+- OB_202311D_NJ.json
+- OB_202311D_QA.json
+- OB_202311D_QJ.json
+- OB_202311N_NA.json
+- OB_202311N_NJ.json
+- OB_202311N_QA.json
+- OB_202311N_QJ.json
+- OB_202311P_NA.json
+- OB_202311P_NJ.json 
+- OB_202311S_QA.json (možná není generován)
+- OB_202311S_QJ.json (možná není generován)
+
+některé městské části mají navíc soubory W a X, tedy měly by existovat i:
+
+- OB_202311W_NA.json
+- OB_202311W_NJ.json
+- OB_202311W_QA.json
+- OB_202311W_QJ.json
+- OB_202311X_NA.json
+- OB_202311X_NJ.json
+- OB_202311X_QA.json
+- OB_202311X_QJ.json
--- download.py
+++ download.py
@@ -3,8 +3,14 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
+USER = os.getenv("TSK_USERNAME")
+PASSWORD = os.getenv("TSK_PASSWORD")
 
-DOWNLOAD_FILES = True
+YEARS = [2023]
+START_MONTH = 11
+END_MONTH = 11
+INCLUDE_QUARTERLY = False
+INCLUDE_SECTIONS = False  # úseky
 
 districts = [
     "P01",
@@ -22,22 +28,73 @@ districts = [
     "P18",
     "P22",
 ]
-filenames = ["OR_202311D_NJ", "OR_202311N_NJ"]
 
-if DOWNLOAD_FILES:
+filename_templates = [
+    "OB_YYYYMMD_NA.json",
+    "OB_YYYYMMD_NJ.json",
+    "OB_YYYYMMD_QA.json",
+    "OB_YYYYMMD_QJ.json",
+    "OB_YYYYMMN_NA.json",
+    "OB_YYYYMMN_NJ.json",
+    "OB_YYYYMMN_QA.json",
+    "OB_YYYYMMN_QJ.json",
+    "OB_YYYYMMP_NA.json",
+    "OB_YYYYMMP_NJ.json",
+    "OB_YYYYMMS_QA.json",
+    "OB_YYYYMMS_QJ.json",
+    "OB_YYYYMMW_NA.json",
+    "OB_YYYYMMW_NJ.json",
+    "OB_YYYYMMW_QA.json",
+    "OB_YYYYMMW_QJ.json",
+    "OB_YYYYMMX_NA.json",
+    "OB_YYYYMMX_NJ.json",
+    "OB_YYYYMMX_QA.json",
+    "OB_YYYYMMX_QJ.json",
+]
 
-    USER = os.getenv("TSK_USERNAME")
-    PASSWORD = os.getenv("TSK_PASSWORD")
+if not INCLUDE_QUARTERLY:
+    filename_templates = [
+        filename
+        for filename in filename_templates
+        if not '_Q' in filename
+    ]
 
-    url_template = "https://zps.tsk-praha.cz/puzzle/genmaps/DISTRICT/FILENAME.json"
+if not INCLUDE_SECTIONS:
+    filename_templates = [
+        filename
+        for filename in filename_templates
+        if not 'J.json' in filename
+    ]
 
-    if not os.path.exists("data"):
-        os.makedirs("data/downloaded")
+url_template = "https://zps.tsk-praha.cz/puzzle/genmaps/DISTRICT/FILENAME"
 
-    for district in districts:
-        for filename in filenames:
-            url = url_template.replace("DISTRICT", district).replace("FILENAME", filename)
-            r = requests.get(url, auth=(USER, PASSWORD))
-            with open(f"./data/{district}-{filename}.json", "w", encoding="utf-8") as f:
-                f.write(r.text)
+if not os.path.exists("data"):
+    os.makedirs("data/downloaded")
 
+total_files = len(districts) * len(filename_templates) * ( END_MONTH - START_MONTH + 1 ) * len(YEARS)
+counter = 0
+
+for district_index, district in enumerate(districts):
+    for filename_index, filename in enumerate(filename_templates):
+        for year in YEARS:
+            for month in range(START_MONTH, END_MONTH + 1):
+                filename = filename.replace("YYYY", str(year)).replace(
+                    "MM", str(month).zfill(2)
+                )
+                url = url_template.replace("DISTRICT", district).replace(
+                    "FILENAME", filename
+                )
+                r = requests.get(url, auth=(USER, PASSWORD))
+                # check response status code
+                if r.status_code != 200:
+                    print(f"Error while downloading {district}-{filename}")
+                    continue
+                with open(
+                    f"./data/downloaded/{district}-{filename}.json",
+                    "w",
+                    encoding="utf-8",
+                ) as f:
+                    f.write(r.text)
+
+                counter += 1
+                print(f"Progress: {counter}/{total_files}")
